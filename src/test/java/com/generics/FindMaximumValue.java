c504ea0196a49905c0956c92dda147ff13423f3a
Bhagi <puppyreddy1997@gmail.com>
2020-10-09 14:35:44 +0530

    [Bhagyalaxmi]Added:finding the maximum integer value at second position

--- src/test/java/com/generics/FindMaximumValue.java
+++ src/test/java/com/generics/FindMaximumValue.java
@@ -6,9 +6,15 @@ import org.junit.Test;
 public class FindMaximumValue {
 
     @Test
-    public void givenThreeIntegers_WhenFirstPositionIsMax_ShouldReturnFirstNumber(){
+    public void givenThreeIntegers_WhenFirstPositionIsMax_ShouldReturnSameNumber(){
         FindMaximum findMaximum = new FindMaximum();
        Integer maxValue = findMaximum.MaximumNumber(9,6,3);
+        Assert.assertSame(9,maxValue);
+    }
+    @Test
+    public void givenThreeIntegers_WhenSecondPositionIsMax_ShouldReturnSameNumber(){
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maxValue = findMaximum.MaximumNumber(6,9,3);
         Assert.assertSame(9,maxValue);
     }
 }
